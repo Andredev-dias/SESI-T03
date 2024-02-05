4ca164d8b5144bf7dcdf3ec5bf9703ce596bcdd7
Andredev-dias <andredias.curitiba@gmail.com>
2024-02-05 11:09:28 -0300

    segunda parte

--- javascript-html-css/script.js
+++ javascript-html-css/script.js
@@ -1,8 +1,7 @@
-function soma() {
+function somar() {
  var num1 = document.getElementById("num1").valueAsNumber;
  var num2 = document.getElementById("num2").valueAsNumber;
 
  var result = num1 + num2;
- document.getElementById("resultado").textContent = result;
- console.log(result)
+ document.getElementById("resultadoSome").textContent = result;
 }
