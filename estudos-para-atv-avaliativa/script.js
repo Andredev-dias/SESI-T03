66e6c8ac694e315f9b6063425cbda6514f94beb4
Andredev-dias <andredias.curitiba@gmail.com>
2024-02-08 08:47:19 -0300

    contato

--- estudos-para-atv-avaliativa/script.js
+++ estudos-para-atv-avaliativa/script.js
@@ -2,5 +2,27 @@ var nomeGlobal;
 var mensagemGlobal;
 
 function conferirMensagemWhatsApp(){
-    
+    var nome = document.getElementById("nome").value;
+    var mensagem = document.getElementById("mensagem").value;
+   
+    nomeGlobal = nome;
+    mensagemGlobal = mensagem;
+
+    document.getElementById("confNome").textContent = nome;
+    document.getElementById("confMsg").textContent = mensagem;
+}
+
+function enviar(){
+    var numeroTelefone = "5541999999999"
+
+    var linkWhatsApp = 
+    "https://wa.me/" + 
+    numeroTelefone + 
+    "?text=Nome:" + 
+    nomeGlobal + 
+    " - " + 
+    mensagemGlobal;
+
+    window.open(linkWhatsApp, "_blank")
+   
 }
