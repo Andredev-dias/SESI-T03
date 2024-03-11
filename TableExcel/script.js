70ca75dd7d42acc8d0fdbed2a1b25d4782e082b1
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-11 08:59:24 -0300

    opa

---
+++ TableExcel/script.js
@@ -0,0 +1,25 @@
+function adicionarItem(){
+    //obter os valores de entrada
+    var nome = document.getElementById("nome").value;
+    var valor = document.getElementById("valor").value; 
+    var quantidade = document.getElementById("quantidade").value; 
+
+    //validar se os campos estão preenchidos
+    if(!nome || !valor || !quantidade){
+        alert("Preencha todos os campos")
+        return;
+    }
+
+    //criar e atribuir as celulas os valores digitados
+    var tabela = document.getElementById("tabela").getElementsByTagName("tbody")[0];
+    //criando uma linha na tabela
+    var novaLinha = tabela.insertRow(tabela.rows.length);
+    //criando celulas dentro da linha
+    var celulaNome = novaLinha.insertCell(0);
+    var celulaValor = novaLinha.insertCell(1);
+    var celulaQuantidade = novaLinha.insertCell(2);
+    //inserindo o valor dos inputs nas celulas
+    celulaNome.innerHTML = nome;
+    celulaValor.innerHTML = valor;
+    celulaQuantidade.innerHTML = quantidade;
+}
