4dcc64a79990ab1380feda9323581433e7452d1b
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-19 11:12:26 -0300

    rep subido

--- localStorageProject/Loja/script.js
+++ localStorageProject/Loja/script.js
@@ -4,11 +4,9 @@ window.onload = function () {
     var storedUser = localStorage.getItem("usuario")
     var user = JSON.parse(storedUser)
 
-
     document.getElementById("user").textContent = user.name;
     document.getElementById("perfil").textContent = user.name;
     document.getElementById("idPerfil").textContent = user.id;
     document.getElementById("novaData").textContent = user.dataEntrada;
-
 }
 
