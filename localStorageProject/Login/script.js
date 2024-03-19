0581b92cbf4f06ec6fa8ea709a19e8c0156c8f9e
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-19 10:59:48 -0300

    subindo pro tony

--- localStorageProject/Login/script.js
+++ localStorageProject/Login/script.js
@@ -8,6 +8,18 @@ const showPassword = () => {
     }
 }
 
+function formatarData(item){
+    var options = {
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    }
+
+    return item.toLocaleString("pt-BR", options)
+}
+
 function login(){
     var nome = $("#nome").val();
     var senha = $("#senha").val();
@@ -15,8 +27,14 @@ function login(){
     if(nome && senha && nome === "admin" && senha === "12345"){
         const user = {
             name: nome,
-            dataEntrada: new Date(),
-            id: Math.floor(Math.randon() * 100000),
+            dataEntrada: formatarData(new Date()),
+            id: Math.floor(Math.random() * 100000),
         }
+        localStorage.setItem("usuario", JSON.stringify(user))
+        window.location.href = "../Loja"
+    }else{
+        document.getElementById("error-modal").style.display = "flex";
     }
 }
+
+const fecharError = () =>   document.getElementById("error-modal").style.display = "none";
---
+++ localStorageProject/Loja/script.js
@@ -0,0 +1,14 @@
+let produtos;
+
+window.onload = function () {
+    var storedUser = localStorage.getItem("usuario")
+    var user = JSON.parse(storedUser)
+
+
+    document.getElementById("user").textContent = user.name;
+    document.getElementById("perfil").textContent = user.name;
+    document.getElementById("idPerfil").textContent = user.id;
+    document.getElementById("novaData").textContent = user.dataEntrada;
+
+}
+
